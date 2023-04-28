438eef0f70bbdc08d937b60231dc58abb5c275e3
Marcelo Fabiano <marcelofabianov@gmail.com>
2023-04-28 16:59:27 -0300

    chore: update index.ts

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Zop
+# Zod
 
 Testando o zod!
+
+## Instalação
+
+```bash
+npm install
+```
+
+### Execução
+
+```bash
+npx ts-node src/index.ts
+```
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as z from 'zod';
 
 const PersonSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  age: z.number().int().positive()
+  name: z.string().trim().min(5).max(255),
+  email: z.string().trim().email().min(5).max(255),
+  age: z.number().int().positive().min(18).max(120),
 });
 
 type Person = z.infer<typeof PersonSchema>;
